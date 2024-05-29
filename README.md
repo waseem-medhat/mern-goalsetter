e1dd9c2b77199626b258b2dc05bfd30d75244bb1
waseem-medhat <wipdev.tech@gmail.com>
2024-05-30 00:08:17 +0300

    Attach goal controller to MongoDB

---
+++ README.md
@@ -0,0 +1,8 @@
+# Goalsetter
+
+This code is based on a tutorial by Traversy Media: <https://www.youtube.com/playlist?list=PLillGF-RfqbbQeVSccR9PGKHzPJSWqcsm>
+
+TODO:
+- Complete tutorial
+- Convert CJS to ESM
+- Convert JS to TS
--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,12 @@
 const asyncHandler = require('express-async-handler')
+const Goal = require('../models/goalModel')
 
 // @desc   Get goals
 // @route  GET /api/goals
 // @access Private
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "GET GOALS" })
+    const goals = await Goal.find()
+    res.status(200).json(goals)
 })
 
 // @desc   Set a goal
@@ -16,21 +18,39 @@ const setGoal = asyncHandler(async (req, res) => {
         throw new Error("Please add text")
     }
 
-    res.status(200).json({ message: "SET GOAL" })
+    const goal = await Goal.create({ text: req.body.text })
+
+    res.status(200).json(goal)
 })
 
 // @desc   Update a goal
 // @route  PUT /api/goals/:id
 // @access Private
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `UPDATE GOAL ${req.params.id}` })
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error("Goal not found")
+    }
+
+    const newGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+    res.status(200).json(newGoal)
 })
 
 // @desc   Delete a goal
 // @route  DELETE /api/goals/:id
 // @access Private
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `DELETE GOAL ${req.params.id}` })
+    if (!req.params.id) {
+        res.status(400)
+        throw new Error("No ID provided")
+    }
+
+    const deleteRes = await Goal.findByIdAndDelete(req.params.id)
+    
+    res.status(200).json(deleteRes)
 })
 
 module.exports = {
